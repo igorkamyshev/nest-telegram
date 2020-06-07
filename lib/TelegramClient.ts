9df841e8d6a94c0f0db1e0e19500a06e5fe1a4ca
Igor Kamyshev <igor@kamyshev.me>
2020-06-07 09:10:13 +0700

    feat: add options to telegram send message

--- lib/TelegramClient.ts
+++ lib/TelegramClient.ts
@@ -1,5 +1,6 @@
 import { Injectable, Inject } from '@nestjs/common';
 import { Chat } from 'telegraf/typings/telegram-types';
+import { merge } from 'lodash';
 
 import { TokenInjectionToken } from './TokenInjectionToken';
 import { TelegramModuleOptionsFactory } from './TelegramModuleOptionsFactory';
@@ -21,17 +22,26 @@ export class TelegramClient {
   public async sendMessage(
     chatId: string | number,
     text: string,
+    options: object,
   ): Promise<void> {
-    await this.telegram.sendMessage(chatId, text);
+    await this.telegram.sendMessage(chatId, text, options);
   }
 
   public async sendMarkdown(
     chatId: string | number,
     markdown: string,
+    options: object,
   ): Promise<void> {
-    await this.telegram.sendMessage(chatId, markdown, {
-      parse_mode: 'Markdown',
-    });
+    await this.telegram.sendMessage(
+      chatId,
+      markdown,
+      merge(
+        {
+          parse_mode: 'Markdown',
+        },
+        options,
+      ),
+    );
   }
 
   public async getChat(chatId: string | number): Promise<Chat> {
